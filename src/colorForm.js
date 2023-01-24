ec7ab4bfd26bf9202f05d9d0f2cfa87b5f9d1a91
Juleian Renteria <Jprenteria02@gmail.com>
2023-01-23 16:07:18 -0800

    completed assignment, cant get colors to render

---
+++ src/colorForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm';
+import './App.css';
+
+
+function App(){
+    let colors = [
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ]
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock key={i} color={color} />
+        )
+    })
+    
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
+
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm addColor={addColor} />
+        </div>
+    )
+
+}
+
+
+export default App;
